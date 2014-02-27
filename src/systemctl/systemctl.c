albfan/systemd-ubuntu-with-dbus

commit aa57b6c5c3eafc84a95f08ac4038d9d93ac7092e

systemctl: mangle names when avoiding dbus
Unit names were mangled in function enable_unit only when dbus was
used. This patch adds mangling also when the dbus is not in use.

This makes it possible to say e.g.:
   systemctl --root=/path enable cups
without spelling cups.service out in full.

src/systemctl/systemctl.c
@@ -4208,33 +4208,37 @@ static int enable_unit(DBusConnection *bus, char **args) {
         _cleanup_dbus_error_free_ DBusError error;
         _cleanup_strv_free_ char **mangled_names = NULL;
 
-        dbus_error_init(&error);
+        if (!args[1])
+                return 0;
+
+        r = mangle_names(args+1, &mangled_names);
+        if (r < 0)
+                goto finish;
 
         r = enable_sysv_units(args);
         if (r < 0)
                 return r;
 
-        if (!args[1])
-                return 0;
+        dbus_error_init(&error);
 
         if (!bus || avoid_bus()) {
                 if (streq(verb, "enable")) {
-                        r = unit_file_enable(arg_scope, arg_runtime, arg_root, args+1, arg_force, &changes, &n_changes);
+                        r = unit_file_enable(arg_scope, arg_runtime, arg_root, mangled_names, arg_force, &changes, &n_changes);
                         carries_install_info = r;
                 } else if (streq(verb, "disable"))
-                        r = unit_file_disable(arg_scope, arg_runtime, arg_root, args+1, &changes, &n_changes);
+                        r = unit_file_disable(arg_scope, arg_runtime, arg_root, mangled_names, &changes, &n_changes);
                 else if (streq(verb, "reenable")) {
-                        r = unit_file_reenable(arg_scope, arg_runtime, arg_root, args+1, arg_force, &changes, &n_changes);
+                        r = unit_file_reenable(arg_scope, arg_runtime, arg_root, mangled_names, arg_force, &changes, &n_changes);
                         carries_install_info = r;
                 } else if (streq(verb, "link"))
-                        r = unit_file_link(arg_scope, arg_runtime, arg_root, args+1, arg_force, &changes, &n_changes);
+                        r = unit_file_link(arg_scope, arg_runtime, arg_root, mangled_names, arg_force, &changes, &n_changes);
                 else if (streq(verb, "preset")) {
-                        r = unit_file_preset(arg_scope, arg_runtime, arg_root, args+1, arg_force, &changes, &n_changes);
+                        r = unit_file_preset(arg_scope, arg_runtime, arg_root, mangled_names, arg_force, &changes, &n_changes);
                         carries_install_info = r;
                 } else if (streq(verb, "mask"))
-                        r = unit_file_mask(arg_scope, arg_runtime, arg_root, args+1, arg_force, &changes, &n_changes);
+                        r = unit_file_mask(arg_scope, arg_runtime, arg_root, mangled_names, arg_force, &changes, &n_changes);
                 else if (streq(verb, "unmask"))
-                        r = unit_file_unmask(arg_scope, arg_runtime, arg_root, args+1, &changes, &n_changes);
+                        r = unit_file_unmask(arg_scope, arg_runtime, arg_root, mangled_names, &changes, &n_changes);
                 else
                         assert_not_reached("Unknown verb");
 
@@ -4292,10 +4296,6 @@ static int enable_unit(DBusConnection *bus, char **args) {
                 }
 
                 dbus_message_iter_init_append(m, &iter);
-
-                r = mangle_names(args+1, &mangled_names);
-                if(r < 0)
-                        goto finish;
 
                 r = bus_append_strv_iter(&iter, mangled_names);
                 if (r < 0) {

commit c969fb1d79c7ea5813d757d4d7ddafda679465ae

systemctl: fix segfault in enable_unit()
This bug was introduced in aa57b6c5c3eafc84a95f08ac4038d9d93ac7092e

We need to initialize the DBusError struct before it is used/freed.

src/systemctl/systemctl.c
@@ -4244,6 +4244,8 @@ static int enable_unit(DBusConnection *bus, char **args) {
         _cleanup_dbus_error_free_ DBusError error;
         _cleanup_strv_free_ char **mangled_names = NULL;
 
+        dbus_error_init(&error);
+
         if (!args[1])
                 return 0;
 
@@ -4257,8 +4259,6 @@ static int enable_unit(DBusConnection *bus, char **args) {
 
         if (!args[1])
                 return 0;
-
-        dbus_error_init(&error);
 
         if (!bus || avoid_bus()) {
                 if (streq(verb, "enable")) {