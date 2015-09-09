67d82c6cbf3b22a418da02416c01cd79bfb46e13
Martin Pitt <martin.pitt@ubuntu.com>
2015-09-09 09:11:41 +0200

    Rework networkd ifupdown integration
    Drop networkd-if-up.d@.service and its invocation from net.agent again. It does
    not work when restarting networkd and requires too much overhead.
    
    Instead, add a suid root helper systemd-networkd-dispatcher which calls
    run-parts, and patch networkd to run the wrapper at the appropriate time.
    
    Add reference to LP: #1492129.

---
+++ debian/extra/systemd-networkd-dispatcher.c
@@ -0,0 +1,58 @@
+/***
+  suid helper to run /etc/network/if-*.d/ hooks from networkd
+
+  (C) 2015 Canonical Ltd.
+  Author: Martin Pitt <martin.pitt@ubuntu.com>
+
+  systemd is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  systemd is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with systemd; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+        const char* action, *interface;
+        const char *c;
+        char path[100];
+
+        if (argc != 3 || (strcmp(argv[1], "up") != 0 && strcmp(argv[1], "post-down") != 0)) {
+                fprintf(stderr, "Usage: %s up|post-down <interface>\n", argv[0]);
+                return 1;
+        }
+        action = argv[1];
+        interface = argv[2];
+
+        /* disallow control chars, spaces, or quotes, as this is passed to shell scripts */
+        for (c = interface; *c != '\0'; ++c)
+                if (isspace(*c) || iscntrl(*c) || *c == '\'' || *c == '"') {
+                        fprintf(stderr, "Invalid character '%c' in interface name\n", *c);
+                        return 1;
+                }
+
+        //printf("action: %s iface %s\n", action, interface);
+        snprintf(path, sizeof(path), "/etc/network/if-%s.d", action);
+
+        /* Run the actual command in the background, to avoid blocking networkd */
+        if (daemon(0, 1) < 0) {
+                perror("Failed to daemonize");
+                return 1;
+        }
+        setenv("IFACE", interface, 1);
+        execl("/bin/run-parts", "/bin/run-parts", "--lsbsysinit", path, NULL);
+        perror("failed to execute run-parts");
+        return 1;
+}
